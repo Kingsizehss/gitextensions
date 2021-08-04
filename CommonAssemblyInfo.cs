cc3341e8d81d3f065e4e55d419835269d9a3f69a
Kingsizehss <Karlsson_Pontus95@hotmail.com>
2021-08-04 03:46:59 +0200

    Just learning.
    Signed-off-by: Kingsizehss <Karlsson_Pontus95@hotmail.com>

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Git Extensions")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyConfiguration("5.0.0")]
 [assembly: AssemblyCompany("Git Extensions")]
 [assembly: AssemblyProduct("Git Extensions")]
 [assembly: AssemblyCopyright("Copyright © 2008-2019 Git Extensions Team")]
